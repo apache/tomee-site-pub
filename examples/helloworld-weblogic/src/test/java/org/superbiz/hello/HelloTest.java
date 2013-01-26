2ebd5715e2606c260a5257e69b1b3bd0a84d3dc9
David Blevins <dblevins@apache.org>
2013-01-26 18:18:49 +0000

    Publishing svnmucc operation to tomee site by dblevins
    git-svn-id: https://svn.apache.org/repos/infra/websites/production/tomee/content@848083 90ea9780-b833-de11-8433-001ec94261de

---
+++ examples/helloworld-weblogic/src/test/java/org/superbiz/hello/HelloTest.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.superbiz.hello;
+
+import junit.framework.TestCase;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
+
+/**
+ * @version $Revision: 1227481 $ $Date: 2012-01-05 04:39:55 +0000 (Thu, 05 Jan 2012) $
+ */
+public class HelloTest extends TestCase {
+
+    public void test() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.core.LocalInitialContextFactory");
+        InitialContext initialContext = new InitialContext(properties);
+
+        HelloEjbLocalHome localHome = (HelloEjbLocalHome) initialContext.lookup("MyHello");
+        HelloEjbLocal helloEjb = localHome.create();
+
+        String message = helloEjb.sayHello();
+
+        assertEquals(message, "Hello, World!");
+    }
+}
