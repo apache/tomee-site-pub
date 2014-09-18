fa4d7a111b7442c20dcc0bdffadcabea8fe7ec8a
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-09-18 16:49:24 +0000

    Publishing svnmucc operation to tomee site by andygumbrecht
    git-svn-id: https://svn.apache.org/repos/infra/websites/production/tomee/content@922717 90ea9780-b833-de11-8433-001ec94261de

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	var location = escape(window.localtion.href).replace("+", "%2B").replace("/", "%2F");
+	//var location = escape(window.location.href).replace("+", "%2B").replace("/", "%2F");
+	var location = encodeURIComponent(window.location.href);
 	$('fb_share_link').attr('share_url', location);
 	$('fb_share_link').attr('share_url', 'http://twitter.com/share?url=' + location + "&via=OpenEJB");
-}
+});
